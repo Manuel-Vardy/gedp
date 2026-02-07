7840058adcb8c710681c2bdbdd9d0096be11fa55
Manuel Vardy <manuelvardy83@gmail.com>
2026-02-07 09:43:57 +0000

    ver 2.1

--- vite.config.js
+++ vite.config.js
@@ -2,16 +2,19 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
+    base: '/',
     build: {
+        outDir: 'dist',
+        emptyOutDir: true,
         rollupOptions: {
             input: {
                 main: resolve(__dirname, 'index.html'),
                 modules: resolve(__dirname, 'modules.html'),
-                training: resolve(__dirname, 'training.html'),
                 blog: resolve(__dirname, 'blog.html'),
                 about: resolve(__dirname, 'about.html'),
                 portals: resolve(__dirname, 'portals.html'),
             },
         },
     },
+    publicDir: 'public',
 });
